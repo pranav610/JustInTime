c9a116dfb7a1cf2fbbe2323f43fcf58fed572a06
sidvisw <90087800+sidvisw@users.noreply.github.com>
2022-04-16 15:31:18 +0530

    First commit

--- SoftwareEngineeringProject/Backend/__init__.py
+++
@@ -1,5 +0,0 @@
-from Backend.Inventory import Inventory
-from Backend.Item import Item
-from Backend.Manufacturer import Manufacturer
-from Backend.Login import Login
-from Backend.Owner import Owner
